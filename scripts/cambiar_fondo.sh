6dd56c2ea9e8c4a5e1941d72c0944186d7fcec53
davidmontes002 <VaddiTesmon0.2@outlook.com>
2026-03-31 16:09:14 -0700

    gestor de wallpapers

---
+++ scripts/cambiar_fondo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Recibimos la imagen desde Rofi
+IMAGEN="$1"
+
+# 1. Aplicamos el fondo con awww
+awww img "$IMAGEN" --transition-type wipe
+
+# 2. Extraemos colores con Pywal
+wal -q -i "$IMAGEN"
+
+# 3. Reiniciamos Waybar para que lea los nuevos colores
+killall waybar ; hyprctl dispatch exec waybar
