8056e0d28fa694577e39089649bdf384668247a5
davidmontes002 <VaddiTesmon0.2@outlook.com>
2026-04-18 09:39:54 -0700

    Limpieza

--- scripts/cambiar_fondo.sh
+++ scripts/cambiar_fondo.sh
@@ -2,27 +2,29 @@
 IMAGEN="$1"
 LOG="/tmp/fondo.log"
 
-echo "=== INICIO ===" > $LOG
-echo "Imagen: $IMAGEN" >> $LOG
+echo "=== INICIO ===" >$LOG
+echo "Imagen: $IMAGEN" >>$LOG
 
 # 1. Fondo
-echo "Aplicando fondo..." >> $LOG
-awww img "$IMAGEN" --transition-type wipe >> $LOG 2>&1
+echo "Aplicando fondo..." >>$LOG
+awww img "$IMAGEN" --transition-type wipe >>$LOG 2>&1
 
-# 2. Matugen
-echo "Ejecutando Matugen..." >> $LOG
+# 2. Matugen (Genera Waybar, Rofi, Kitty y bordes de Hyprland)
+echo "Ejecutando Matugen..." >>$LOG
 mkdir -p "$HOME/.cache/colors"
-matugen image "$IMAGEN" -m dark --prefer saturation -c "$HOME/dotfiles/matugen/.config/matugen/config.toml" >> $LOG 2>&1
+matugen image "$IMAGEN" -m dark --prefer saturation -c "$HOME/dotfiles/matugen/.config/matugen/config.toml" >>$LOG 2>&1
 
-# 3. Waybar
-echo "Reiniciando Waybar..." >> $LOG
+# 3. Waybar e Hyprland
+echo "Reiniciando entorno..." >>$LOG
 killall waybar
 sleep 1
-nohup waybar >> $LOG 2>&1 &
+nohup waybar >>$LOG 2>&1 &
+hyprctl reload >>$LOG 2>&1
 
 # 4. Notificaciones y Terminal
-echo "Actualizando extras..." >> $LOG
-pkill -USR1 kitty >> $LOG 2>&1
-swaync-client -rs >> $LOG 2>&1
+echo "Actualizando extras..." >>$LOG
+touch "$HOME/.config/kitty/kitty.conf"
+pkill -USR1 kitty >>$LOG 2>&1
+swaync-client -rs >>$LOG 2>&1
 
-echo "=== FIN ===" >> $LOG
+echo "=== FIN ===" >>$LOG
